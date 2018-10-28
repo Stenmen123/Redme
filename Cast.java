1d9717a11381682d4040197028796d17110add06
Stenmen123 <44323342+Stenmen123@users.noreply.github.com>
2018-10-28 20:30:59 +0200

    Add files via upload

---
+++ Cast.java
@@ -0,0 +1,8 @@
+package com.hillel.Task2;
+
+public class Cast {
+    public static int div(double x, double y,double c){
+        double result = x + y *(c/2);
+        return (int) result;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.hillel.Task2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double result = Cast.div(12,4,8);
+        System.out.println(result);
+	// write your code here
+    }
+}
